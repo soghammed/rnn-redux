7b96ecb7a745c0c57d99cecc0d6ab7098e6d871d
sog <mohammed.hammed@parlicentre.org>
2019-04-29 11:45:39 +0100

    redo initial commit

---
+++ android/app/src/main/java/com/rnnv2/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rnnv2;
+
+// import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.NavigationActivity;
+
+// public class MainActivity extends ReactActivity {
+
+//     /**
+//      * Returns the name of the main component registered from JavaScript.
+//      * This is used to schedule rendering of the component.
+//      */
+//     @Override
+//     protected String getMainComponentName() {
+//         return "RNNv2";
+//     }
+// }
+
+
+public class MainActivity extends NavigationActivity {
+
+}
